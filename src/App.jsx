1b66c091733465617fc7fd95abf634ffad8d3c41
shahzaib2k3 <shahzaibutt2003@gmail.com>
2026-07-15 14:42:51 +0500

    day 3 done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Game from "./components/Game";
+import Counter from "./components/Counter";
+import { useState } from "react";
 /*const profiles = [
   { name: "Shahzaib", role: "Aspiring MERN Developer", image: "https://via.placeholder.com/100" },
   { name: "Alex", role: "Backend Engineer", image: "https://via.placeholder.com/100" },
@@ -9,22 +11,32 @@ import Game from "./components/Game";
   { name: "Shahzaib",role:"freelancer",image:"https://via.placeholder.com/100"}
 ];
 */
-const games =[
-  {name:"gta5",desc:" open world game",image:"https://via.placeholder.com/100"},
-  {name:"cod",desc:" war game ",image:"https://via.placeholder.com/100"},
-  {name:"fifa",desc:" football game ",image:"https://via.placeholder.com/100"},
-  {name:"far cry 5",desc:" tactics game ",image:"https://via.placeholder.com/100"}
-]
+
 function App() {
+  const [games,setGames] =useState([
+  {name:"gta5",desc:" open world game",image:"https://via.placeholder.com/100",id:1},
+  {name:"cod",desc:" war game ",image:"https://via.placeholder.com/100",id:2},
+  {name:"fifa",desc:" football game ",image:"https://via.placeholder.com/100",id:3},
+  {name:"far cry 5",desc:" tactics game ",image:"https://via.placeholder.com/100",id:4}
+])
+function addgame(){
+  return (
+    setGames([...games,{name:"need for speed",desc:"racing game ",image:"https://via.placeholder.com/100",id:5}])
+  )
+}
   return (
     <>
     <Header/>
     <h1>Hello, I'm building this myself</h1>
-    {games.map((game,index)=>(
-      <Game key={index} name={game.name} desc={game.desc} image={game.image} />
+    {games.map((game)=>(
+      <Game key={game.id} name={game.name} desc={game.desc} image={game.image} />
+      
 
     ))}
-
+    <button onClick={addgame}>
+      add game 
+    </button>
+     <Counter/>
     <Footer/>
     </>
   );
---
+++ src/components/Counter.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+function Counter (){
+ const [count,setCount]= useState(0)
+
+ function increment(){
+setCount(count+1)
+ }
+
+ function decrement (){
+setCount(count-1)
+ }
+    
+    return (
+        <div>
+            <h2>current count is {count}</h2>
+            <button onClick={()=>increment()}> increment </button>
+            <button onClick={()=>decrement()}> decrement </button>
+        </div>
+
+    )
+}
+
+export default Counter
